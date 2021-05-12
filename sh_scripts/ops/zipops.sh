8a539765819a32146a5562f244d4d73f4a4619ca
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-05-12 08:43:25 +0200

    New shell-directory structure and new sh to unzip ops

---
+++ sh_scripts/ops/zipops.sh
@@ -0,0 +1,18 @@
+mkdir -p ops{2007..2021}
+unzip ops2007.zip -d ops2007
+unzip ops2008.zip -d ops2008
+unzip ops2009.zip -d ops2009
+unzip ops2010.zip -d ops2010
+unzip ops2011.zip -d ops2011
+unzip ops2012.zip -d ops2012
+unzip ops2013.zip -d ops2013
+unzip ops2014.zip -d ops2014
+unzip ops2015.zip -d ops2015
+unzip ops2016.zip -d ops2016
+unzip ops2017.zip -d ops2017
+unzip ops2018.zip -d ops2018
+unzip ops2019.zip -d ops2019
+unzip ops2020.zip -d ops2020
+unzip ops2021.zip -d ops2021
+mkdir -p opszip
+mv *.zip opszip
