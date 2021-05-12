bc2cb4b58adfefc01136c1c59ee6c80fadf3c96e
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-05-12 09:46:48 +0200

    New file to convert from ISO-8859-15 to UTF-8

---
+++ sh_scripts/ops/ops_iso_to_utf.sh
@@ -0,0 +1,4 @@
+iconv -f ISO-8859-15 -t UTF-8//TRANSLIT < codesamtl2009.txt > ops2009syst_kodes.txt
+iconv -f ISO-8859-15 -t UTF-8//TRANSLIT < dreistamtl2009.txt > ops2009syst_dreisteller.txt
+iconv -f ISO-8859-15 -t UTF-8//TRANSLIT < gruppenamtl2009.txt > ops2009syst_gruppen.txt
+iconv -f ISO-8859-15 -t UTF-8//TRANSLIT < kapitelamtl2009.txt > ops2009syst_kapitel.txt
