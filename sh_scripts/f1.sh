eb851f6fd50e2f9fe47977c87b43308daa4b15ed
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-06-09 15:40:23 +0200

    New f1 to convert file from ISO to UTF-8

--- sh_scripts/f1.sh
+++ sh_scripts/f1.sh
@@ -3,3 +3,6 @@ awk -F 'separator_in_file' 'BEGIN{OFS = "separator_output"}{print $1, $2, $3, $4
 
 # sed to remove/replace windows end of line
 sed -e 's/^M/patern/' file_to_read > file_to_write
+
+# iconv to convert from ISO-8859-15 (Windows) to utf-8
+iconv -f ISO-8859-15 -t UTF-8//TRANSLIT < FILE_TO_READ > FILE_OUPUT
