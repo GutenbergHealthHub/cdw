c4723e0ed26e3e36718a8db3c5037bcadf7b7771
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-07-20 13:28:15 +0200

    New function added to show string_objects and decimal_objects

--- sql_scripts/copra/copra_biosignals.sql
+++ sql_scripts/copra/copra_biosignals.sql
@@ -22,9 +22,14 @@ create or replace function copra.patient_string(config_id bigint)
           --s.id,
 	      patid,
           date_part('year', geb)::int geb,
-          (date_part('year', datetimeto) - date_part('year', geb))::int,
+          (date_part('year', now()) - date_part('year', geb))::int,
           geschlecht,
-          c.name param,
+	  --c.name param,
+          case 
+            when displayname notnull then displayname
+            when displayname isnull and description notnull then regexp_replace(description, '_', ' ', 'g')
+            when displayname isnull and description isnull then regexp_replace(c.name, '_', ' ', 'g')
+          end param,
           val wert,
           datetimeto
         from copra.co6_medic_data_patient p
@@ -38,7 +43,6 @@ create or replace function copra.patient_string(config_id bigint)
         and val not like ''
         and s.varid = config_id;
   end;$$;
-
 -- Patient Decimal Parameters
 
 drop function if exists copra.patient_decimal(config_id bigint);
@@ -66,7 +70,12 @@ create or replace function copra.patient_decimal(config_id bigint)
           date_part('year', geb)::int geb,
           (date_part('year', datetimeto) - date_part('year', geb))::int,
           geschlecht,
-          c.name param,
+          --c.name param,
+          case
+            when displayname notnull then displayname
+            when displayname isnull and description notnull then regexp_replace(description, '_', ' ', 'g')
+            when displayname isnull and description isnull then regexp_replace(c.name, '_', ' ', 'g')
+          end param,
           val,
           c.unit,
           datetimeto
@@ -90,15 +99,15 @@ create or replace function copra.patient_pressure(config_id bigint)
     pat_h_id varchar,
     pat_b_year int,
     pat_age int,
-    pat_gender varchar,
+    gender varchar,
     meassurement varchar,
-    syst numeric,
+    systolic numeric,
     unit_syst varchar,
-    diast numeric,
+    diastolic numeric,
     unit_diast varchar,
     means numeric,
     unit_means varchar,
-    meassurement_date timestamp
+    datetimeto timestamp
   )
   language plpgsql
   as $$
@@ -106,18 +115,23 @@ create or replace function copra.patient_pressure(config_id bigint)
   begin
 	  return query
 	    select
-          patid,
+          p.patid,
           date_part('year', geb)::int geb,
-          (date_part('year', datetimeto) - date_part('year', geb))::int,
+          (date_part('year', pr.datetimeto) - date_part('year', geb))::int,
           geschlecht,
-          c.name param,
-          systolic syst,
+          --c.name param,
+          case
+            when displayname notnull then displayname
+            when displayname isnull and description notnull then regexp_replace(description, '_', ' ', 'g')
+            when displayname isnull and description isnull then regexp_replace(c.name, '_', ' ', 'g')
+          end param,
+          pr.systolic syst,
           c.unit,
-          diastolic dia,
+          pr.diastolic dia,
           c.unit,
-          mean me,
+          pr.mean me,
           c.unit,
-          datetimeto
+          pr.datetimeto
         from copra.co6_medic_data_patient p
         join copra.co6_medic_pressure pr
           on pr.parent_id = p.id
@@ -128,3 +142,109 @@ create or replace function copra.patient_pressure(config_id bigint)
         and flagcurrent
         and varid = config_id ;
   end;$$;
+
+-- Patient Objects String Parameters
+drop function if exists copra.patient_object_string(bigint);
+
+create or replace function copra.patient_object_string(config_id bigint)
+  returns table (
+     --pat_id bigint,
+    patid varchar,
+    pat_b_year int,
+    pat_age int,
+    gender varchar,
+    meassurement varchar,
+    value varchar,
+    meassurement_date timestamp
+  )
+  language plpgsql
+  as $$
+
+  begin
+	  return query
+        select
+          --s.id,
+          p.patid,
+          date_part('year', geb)::int geb,
+          (date_part('year', now()) - date_part('year', geb))::int,
+          p.geschlecht,
+          case
+            when displayname notnull then displayname
+            when displayname isnull and description notnull then regexp_replace(description, '_', ' ', 'g')
+            when displayname isnull and description isnull then regexp_replace(c.name, '_', ' ', 'g')
+          end param,
+          val wert,
+          datetimeto
+        from copra.co6_data_string s
+        join copra.co6_data_object o
+          on o.id = s.parent_id
+        join copra.co6_data_object op
+          on op.id = o.parent_id
+        join copra.co6_medic_data_patient p
+          on p.id = op.parent_id
+        join copra.co6_config_variables c
+          on c.id = s.varid
+        where op.parent_varid = 1
+        and not s.deleted
+        and not op.deleted
+        and not o.deleted
+        and op.flagcurrent
+        and s.flagcurrent
+        and o.flagcurrent
+        and s.varid = config_id
+   ;
+end;$$;
+
+-- Patient Objects Decimal Parameters
+drop function if exists copra.patient_object_decimal(bigint);
+
+create or replace function copra.patient_object_decimal(config_id bigint)
+  returns table (
+     --pat_id bigint,
+    pat_h_id varchar,
+    pat_b_year int,
+    pat_age int,
+    pat_gender varchar,
+    meassurement varchar,
+    value decimal,
+    unit varchar,
+    meassurement_date timestamp
+  )
+  language plpgsql
+  as $$
+
+  begin
+          return query
+        select
+          --s.id,
+          patid,
+          date_part('year', geb)::int geb,
+          (date_part('year', now()) - date_part('year', geb))::int,
+          geschlecht,
+          case
+            when displayname notnull then displayname
+            when displayname isnull and description notnull then regexp_replace(description, '_', ' ', 'g')
+            when displayname isnull and description isnull then regexp_replace(c.name, '_', ' ', 'g')
+          end param,
+          val wert,
+	  c.unit,
+          datetimeto
+        from copra.co6_data_decimal_6_3 d
+        join copra.co6_data_object o
+          on o.id = d.parent_id
+        join copra.co6_data_object op
+          on op.id = o.parent_id
+        join copra.co6_medic_data_patient p
+          on p.id = op.parent_id
+        join copra.co6_config_variables c
+          on c.id = d.varid
+        where op.parent_varid = 1
+        and not d.deleted
+        and not op.deleted
+        and not o.deleted
+        and op.flagcurrent
+        and d.flagcurrent
+        and o.flagcurrent
+        and d.varid = config_id
+   ;
+end;$$;
