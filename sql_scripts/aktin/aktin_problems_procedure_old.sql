a9161c49cf481de60fd53da962d9f4888b29ccd9
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-07-22 09:19:44 +0200

    Update dqa table, view and procedure to management the problems in the table aktin_copra

--- sql_scripts/aktin/aktin_problems_procedure_old.sql
+++ sql_scripts/aktin/aktin_problems_procedure_old.sql
@@ -1,5 +1,30 @@
 -- Procedure to export the rows with problem into a file
+/*create or replace procedure aktin.pr_aktin_copra_problem()
+  language plpgsql
+  as $$
+  declare 
+    file_date date;
+    file_to_save varchar;
+  begin 
+	  file_date := now()::date;
+	  file_to_save := '/media/db/cdw_database/schema_table_problem/aktin_copra/aktin_copra'||file_date||'.csv';
+	  insert into aktin.aktin_copra_problem
+        select 
+          * 
+        from aktin.v_aktin_copra_problem
+        where zeitpunkt_aufnahme > (select max(zeitpunkt_aufnahme) from aktin.aktin_copra_problem acp) 
+        or (select count(1) from aktin.aktin_copra_problem acp) = 0;
+       
+       execute 'copy (select * from aktin.v_aktin_copra_problem) to ''' || file_to_save || ''' with csv header delimiter '';'''; 
+       
+  end; $$;
 
+ 
+--call aktin.pr_aktin_copra_problem();
+
+--truncate table aktin.aktin_copra_problem restart identity;
+
+*/
 drop procedure if exists aktin.pr_dqa_aktin_copra_problem_to_file();
 
 create or replace procedure aktin.pr_dqa_aktin_copra_problem_to_file()
@@ -11,6 +36,7 @@ create or replace procedure aktin.pr_dqa_aktin_copra_problem_to_file()
   begin 
 	  file_date := now()::date;
 	  file_to_save := '/media/db/cdw_database/schema_table_problem/aktin_copra/aktin_copra'||file_date||'.csv';
+	  --file_to_save := '/home/abel/cdw/schema_table_problem/aktin_copra/aktin_copra'||file_date||'.csv';
 	  insert into aktin.dqa_aktin_copra_problem(problem, problem_value, case_number, admission) 
         select 
           * 
--- sql_scripts/test/aktin_problems_table_view.sql
+++ sql_scripts/test/aktin_problems_table_view.sql
@@ -95,29 +95,36 @@ order by problem
  * 
  * */
 
-drop table if exists aktin.aktin_copra_problem_tmp;
+drop table if exists aktin.dqa_aktin_copra_problem;
 
-create table aktin.aktin_copra_problem_tmp(
+create table aktin.dqa_aktin_copra_problem(
   id bigserial primary key,
   problem varchar not null,
   problem_value varchar not null,
-  fall_nummer   varchar(20) not null,
+  case_number   varchar(20) not null,
+  admission timestamp not null,
   inserted timestamp not null default now(),
-  unique(fall_nummer, problem)
+  unique(case_number, problem, problem_value)
 );
 
 -- view to show aktin copra data with problems 
---create or replace view aktin.aktin_dqa 
---as
+drop view if exists aktin.dqa_v_aktin_copra_problem; 
+
+create or replace view aktin.dqa_v_aktin_copra_problem
+as
   select 
     'alter_aufnahme' problem, 
-    alter_aufnahme::varchar problem_value 
+    alter_aufnahme::varchar problem_value,
+    fall_nummer,
+    zeitpunkt_aufnahme 
   from aktin.aktin_copra 
   where alter_aufnahme not between 0 and 120
     union 
   select 
     'geschlecht',
-    geschlecht 
+    geschlecht,
+    fall_nummer,
+    zeitpunkt_aufnahme 
   from aktin.aktin_copra
   left join metadata_repository.gender g
     on geschlecht = g.sourceid 
@@ -125,7 +132,9 @@ create table aktin.aktin_copra_problem_tmp(
     union
   select 
     'zeitpunkt_aufnahme',
-    zeitpunkt_aufnahme::varchar
+    zeitpunkt_aufnahme::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where zeitpunkt_aufnahme > zeitpunkt_entlassung 
   or zeitpunkt_aufnahme > zeitpunkt_ersteinsch 
@@ -133,27 +142,35 @@ create table aktin.aktin_copra_problem_tmp(
     union 
   select 
     'zeitpunkt_ersteinsch', 
-    zeitpunkt_ersteinsch::varchar
+    zeitpunkt_ersteinsch::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where zeitpunkt_ersteinsch > zeitpunkt_entlassung 
   or zeitpunkt_ersteinsch isnull
     union 
   select 
     'zeitpunkt_entlassung',
-    zeitpunkt_entlassung::varchar
+    zeitpunkt_entlassung::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where zeitpunkt_entlassung < zeitpunkt_aufnahme
     union 
   select 
     'aufnahmegrund',
-    aufnahmegrund 
+    aufnahmegrund,
+    fall_nummer ,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where aufnahmegrund isnull 
   or aufnahmegrund like ''
     union 
   select 
     'mts_status_farbe',
-    mts_status_farbe 
+    mts_status_farbe ,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac
   left join metadata_repository.status_color sc
     on sc.sourceid = ac.mts_status_farbe
@@ -162,61 +179,81 @@ create table aktin.aktin_copra_problem_tmp(
    union 
   select 
     'atemfrequenz',
-    atemfrequenz::varchar
+    atemfrequenz::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where atemfrequenz not between 0 and 99
     union 
   select 
     'herzfrequenz',
-    herzfrequenz::varchar 
+    herzfrequenz::varchar ,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where herzfrequenz not between 0 and 300
     union 
   select 
     'systolic_pressure',
-    systolic_pressure::varchar
+    systolic_pressure::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where systolic_pressure not between 0 and 300
     union 
   select 
     'temperatur', 
-    temperatur::varchar 
+    temperatur::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme 
   from aktin.aktin_copra ac 
   where temperatur not between 0 and 45
     union 
   select 
     'gcs_scorevalue',
-    gcs_scorevalue::varchar 
+    gcs_scorevalue::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme 
   from aktin.aktin_copra ac 
   where gcs_scorevalue not between 3 and 15
     union 
   select
     'gcs_augenint',
-    gcs_augenint::varchar 
+    gcs_augenint::varchar,
+    fall_nummer ,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where gcs_augenint not between 1 and 4
    union 
   select 
     'gcs_motorik',
-    gcs_motorik::varchar 
+    gcs_motorik::varchar ,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where gcs_motorik not between 1 and 4
     union 
   select 
     'gcs_lang',
-    gcs_lang::varchar 
+    gcs_lang::varchar,
+    fall_nummer ,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where gcs_lang not between 1 and 4
     union 
   select 
     'schmerzskala',
-    schmerzskala::varchar
+    schmerzskala::varchar,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where schmerzskala not between 0 and 10
     union 
   select 
     'pupille_reaktion_links',
-    pupille_reaktion_links 
+    pupille_reaktion_links ,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   left join metadata_repository.pupille_reaction pr
     on pr.sourceid = ac.pupille_reaktion_links 
@@ -225,7 +262,9 @@ create table aktin.aktin_copra_problem_tmp(
     union 
   select 
     'pupille_reaktion_rechts', 
-    pupille_reaktion_rechts 
+    pupille_reaktion_rechts ,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   left join metadata_repository.pupille_reaction pr
     on pr.sourceid = ac.pupille_reaktion_rechts 
@@ -234,7 +273,9 @@ create table aktin.aktin_copra_problem_tmp(
     union 
   select 
     'pupille_weite_links', 
-    pupille_weite_links 
+    pupille_weite_links,
+    fall_nummer ,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   left join metadata_repository.pupille_width pw 
     on pw.sourceid = pupille_weite_links 
@@ -243,7 +284,9 @@ create table aktin.aktin_copra_problem_tmp(
     union 
   select 
     'pupille_weite_rechts',
-    pupille_weite_rechts 
+    pupille_weite_rechts,
+    fall_nummer,
+    zeitpunkt_aufnahme 
   from aktin.aktin_copra ac 
   left join metadata_repository.pupille_width pw 
     on pw.sourceid = pupille_weite_rechts 
@@ -252,11 +295,13 @@ create table aktin.aktin_copra_problem_tmp(
     union 
   select 
     'schwanger',
-    schwanger 
+    schwanger ,
+    fall_nummer,
+    zeitpunkt_aufnahme
   from aktin.aktin_copra ac 
   where (geschlecht = 'M' or alter_aufnahme < 18 or alter_aufnahme > 65) 
   and schwanger = 'Patient ist anamnestisch schwanger'
-  order by problem
+  order by fall_nummer 
 ;
 
-
+--select * from aktin.dqa_v_aktin_copra_problem;
