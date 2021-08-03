1763d4d844196f11c304c013b838304d47b25d18
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-08-03 15:09:08 +0200

    New script to save the release information of the icd10gm

--- sh_scripts/icd/etl/icd_etl.sh
+++ sh_scripts/icd/etl/icd_etl.sh
@@ -1,9 +1,10 @@
 # ETL for all icd from 2007 to 2021
 icdversions="/home/$USER/cdw/ICD/icd_versions/"
 icdcodes="${icdversions}codes/"
-bash unziper.sh $icdversions
-bash copy_codes.sh $icdversions
-bash iso_2_utf8.sh $icdcodes
+bash ../../utils/unziper.sh $icdversions
+bash ../../utils/copy_codes.sh $icdversions
+bash ../../utils/extra_info.sh $icdversions
+bash ../../utils/iso_2_utf8.sh $icdcodes
 cd
 echo "End!"
 exit
