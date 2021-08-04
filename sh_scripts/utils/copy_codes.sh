f758e24449391e9f10c2dce917dda5cb16f1a08a
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-08-04 13:44:54 +0200

    Some boberias mas...

--- sh_scripts/utils/copy_codes.sh
+++ sh_scripts/utils/copy_codes.sh
@@ -18,5 +18,6 @@ for d in */*/*/*s20*.txt */*/*/*[sS].txt;do # iteration in directory with codes
   if [[ $d =~ $ver ]]; then # name in directory for version
     icdcodes=`echo $d | sed -e 's/\.txt//g'` # remove .txt
     cp $d codes/${BASH_REMATCH[0]}.txt # copy to directory codes
+    chmod -w codes/${BASH_REMATCH[0]}.txt
   fi
 done
