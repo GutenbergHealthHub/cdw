5408a1f77e1502ad850722f44d3d90849bae7174
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-10-25 13:13:17 +0200

    New table for AKTIN to manager the job

---
+++ sql_scripts/aktin/aktin_jobtable.sql
@@ -0,0 +1,5 @@
+-- Table for job table in aktin
+create table aktin.jobtable_test(
+  fall_nummer varchar, -- irgendwann primary key und foreign key die aus aktin_copra zeigt
+  retrievaldate timestamp -- zeitangabe in trigger zum import
+);
