04c0f25b85fa64be64fa69a0767a73ca1d059df3
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2021-10-26 14:50:50 +0200

    Modifications in table jobtable on schema aktin, modidication on function and trigger to insert information into the jobtable

--- sql_scripts/aktin/aktin_jobtable.sql
+++ sql_scripts/aktin/aktin_jobtable.sql
@@ -1,5 +1,7 @@
 -- Table for job table in aktin
 create table aktin.jobtable_test(
   fall_nummer varchar, -- irgendwann primary key und foreign key die aus aktin_copra zeigt
-  retrievaldate timestamp -- zeitangabe in trigger zum import
+  retrievaldate timestamp, -- zeitangabe in trigger zum import
+  trigger_type varchar, --event (insert or update)
+  event_user varchar -- user
 );
--- sql_scripts/aktin/aktin_jobtable_function_trigger_new.sql
+++ sql_scripts/aktin/aktin_jobtable_function_trigger_new.sql
@@ -1,33 +1,29 @@
 -- procedure to insert new cases and date of insert into the aktin.jobtable_test 
 -- after new insert into the table aktin.aktin_copra 
-create or replace function aktin.to_jobtable()
+create or replace function aktin.aktin_copra_to_jobtable()
   returns trigger 
   language plpgsql
   as $$
     begin 
-	  insert into aktin.jobtable_test 
+	  insert into aktin.jobtable
         select
           ac.fall_nummer,
-          now() retrievaldate
-        from newtable ac -- aktin.aktin_copra, it is defined in the trigger
-        ; -- cases not in jobtable
+          now() retrievaldate,
+          TG_OP, -- trigger event
+          current_user -- user
+        ; 
       return null;
     end;
   $$;
 
--- trigger on aktin.aktin_copra by insert 
-create trigger tr_insert_into_jobtable 
-  after insert 
-    on aktin.aktin_copra 
-    referencing new table as newtable
-    for each statement execute function aktin.to_jobtable()
-;
 
--- trigger on aktin.aktin_copra update 
-create trigger tr_update_to_jobtable 
-  after update 
-    on aktin.aktin_copra 
-    referencing new table as newtable
-    for each statement execute function aktin.to_jobtable()
+
+-- trigger on aktin.aktin_copra by insert or update
+
+-- drop trigger if exists tr_insert_into_jobtable on aktin.aktin_copra;
+
+create trigger tr_insert_into_jobtable
+  after insert or update
+    on aktin.aktin_copra
+    for each row execute function aktin.aktin_copra_to_jobtable()
 ;
- 
