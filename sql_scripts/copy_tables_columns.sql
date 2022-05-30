c83e95554b025a39f646a2ab35f892d62d1fd9ca
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2022-05-30 16:02:11 +0200

    SQL script to copy name of schema, tables and columns to make the views of dqa

---
+++ sql_scripts/copy_tables_columns.sql
@@ -0,0 +1,20 @@
+--copy
+--(
+select t.table_schema , t.table_name, c.column_name 
+from information_schema."columns" c 
+join information_schema."tables" t 
+  on t.table_name = c.table_name 
+where t.table_schema = 'kis'
+and t.table_type = 'BASE TABLE'
+and c.udt_name = 'varchar'
+and c.column_name <> 'FALNR'
+and c.column_name <> 'PATNR'
+and c.column_name <> 'EINRI';
+--) to '/home/abel/test/kis_test/kis_tables_columns.csv' with delimiter E';' csv;
+
+select * from information_schema."tables" t;
+
+select table_schema, table_name, column_name from information_schema."columns" c where table_schema = 'kis' and udt_name like 'varchar';
+select * from information_schema."columns" c where table_schema = 'kis' and udt_name like 'varchar';
+
+drop table copra.co6_config_variables_tmp;
