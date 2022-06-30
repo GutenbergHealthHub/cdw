ebe837bed252a6e100806b02d2832539ac077da1
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2022-06-30 12:58:48 +0200

    Last update Do 30. Jun 12:58:48 CEST 2022

--- pdf-tex_scrips/DQA_KIS_Dokumentation/rm_etc.sh
+++ pdf-tex_scrips/DQA_KIS_Dokumentation/rm_etc.sh
@@ -1,3 +1,33 @@
 # delete extra created files
 # cd $1
-rm *.aux *.l* *.o* *.sy* *.to* *.b* *.r*
+# rm *.aux *.l* *.o* *.sy* *.to* *.b* *.r*
+
+if [ -f DQA_KIS_Dokumentation.aux ]
+then 
+  rm *.aux
+fi
+
+if [ -f DQA_KIS_Dokumentation.log ]
+then
+  rm *.log
+fi
+
+if [ -f DQA_KIS_Dokumentation.lot ]
+then
+  rm *.lot
+fi
+
+if [ -f DQA_KIS_Dokumentation.out ]
+then
+  rm *.out
+fi
+
+if [ -f DQA_KIS_Dokumentation.synctex.gz ]
+then
+  rm *.synctex.gz
+fi
+
+if [ -f DQA_KIS_Dokumentation.toc ]
+then
+  rm *.toc
+fi
